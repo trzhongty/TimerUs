dea440624eeaf7c00a498c86f1caad05a48cc580
钟天运 <trzhongty@163.com>
2019-09-16 21:00:05 +0800

    初次移植完毕，仅预留一个TIMERUS_TASK_NUMMAX 宏定义设置 静态最大任务数量

--- timerus.c
+++ timerus.c
@@ -1,138 +1,207 @@
-#include "timdelay.h"
-#include "tim.h"		//包含所使用的定时结构体外部声明
+/**
+  ******************************************************************************
+  * @file    timerus.c
+  * @author  trzhongty@163.com
+  * @brief   基于单个硬件定时器实现的多任务US级别中断回调软件定时器
+  ******************************************************************************
+  * @version 	0.1
+  * @date		2019年9月12日23:33:12
+  * @note		基于单个硬件定时器实现的多任务US级别中断回调软件定时器
+  * @since		初次创建
+  *
+  @verbatim
+  @endverbatim
+  */
 
+/* Includes ------------------------------------------------------------------*/
+#include "timerus.h"
 
+/** @defgroup TimerUs
+  * @{
+  */
 
-//功能结构体定义及初始化
-TIMDelay_TypeDef htimdelay =
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+/** @defgroup TimerUs Exported Functions
+  * @{
+  */
+
+/**
+  * @brief  初始化定时器结构体
+  * @param  pTimerUs    定时器主结构体指针
+  * @param  TimHandle   硬件定时器句柄（指针）
+  */
+ void TimerUs_Init( TimerUs_TypeDef *pTimerUs, void * TimHandle )
+ {
+     pTimerUs->TaskNum = 0;
+     pTimerUs->WorkNum = 0;
+     pTimerUs->TimHandle = TimHandle;
+ }
+
+/**
+  * @brief  创建一个定时任务
+  * @param  pTimerUs    定时器主结构体指针
+  * @param  pFun    定时中断回调函数,存在一个u8参数，指明任务序号
+  * @return 返回任务序号，小于0表示创建失败
+  */
+int8_t TimerUs_TaskCreate( TimerUs_TypeDef *pTimerUs, void ( *pFun)( uint8_t TaskNum ) )
 {
-	.WorkNum = 0
-};
+    int8_t TaskNum = (int8_t)( pTimerUs->TaskNum ) + 1;
 
-//任务信息注册
-HAL_StatusTypeDef TIMDelayInit( uint8_t TaskNum, void ( *pfun)() )
-{
-	if( TaskNum >= TIMDelay_TASK_NUMMAX )
-		return HAL_ERROR;
-	htimdelay.Task[ TaskNum ].pfun = pfun;
-	htimdelay.Task[ TaskNum ].state = TIMDelay_Ready;
-	return HAL_OK;
+    if ( TaskNum >= TIMERUS_TASK_NUMMAX ||
+         TaskNum < 0 )
+        return -1;
+
+	pTimerUs->Task[ TaskNum ].pfun = pFun;
+	pTimerUs->Task[ TaskNum ].state = TIMERUS_READY;
+    ++(pTimerUs->TaskNum);
+	return 0;
 }
 
-//延时结束，中断回调函数入口
-void TIMDelayIT()
+/**
+  * @brief  硬件定时器中断函数
+  * @param  pTimerUs    定时器主结构体指针
+  */
+void TimerUs_IT( TimerUs_TypeDef *pTimerUs )
 {
 	static uint8_t i;
 	
-	
-	TIMDelayhtim.Instance->CR1 &= ~(TIM_CR1_CEN);	//暂停CNT计数
-	
+	TimerUs_HAL_PauseTim( pTimerUs );	//暂停CNT计数
 	do
 	{
-		htimdelay.Task[ htimdelay.TaskNumTab[ 0 ] ].state = TIMDelay_OK;	//将第一序列任务状态设为OK
+		pTimerUs->Task[ pTimerUs->TaskNumTab[ 0 ] ].state = TIMERUS_OK;	//将第一序列任务状态设为OK
 		
-		if( (htimdelay.Task[ htimdelay.TaskNumTab[ 0 ] ]).use_it )	//如果启用了中断回调，则调用回调函数
-			(htimdelay.Task[ htimdelay.TaskNumTab[ 0 ] ]).pfun();
+		if( (pTimerUs->Task[ pTimerUs->TaskNumTab[ 0 ] ]).use_it )	//如果启用了中断回调，则调用回调函数
+			(pTimerUs->Task[ pTimerUs->TaskNumTab[ 0 ] ]).pfun();
 		
-		--(htimdelay.WorkNum);
-		for( i = 0; i < htimdelay.WorkNum; ++i )		//执行完一个，序列前移，
+		--(pTimerUs->WorkNum);
+		for( i = 0; i < pTimerUs->WorkNum; ++i )		//执行完一个，序列前移，
 		{
-			htimdelay.TaskarrTab[ i ] = htimdelay.TaskarrTab[ i + 1 ];
-			htimdelay.TaskNumTab[ i ] = htimdelay.TaskNumTab[ i + 1 ];
+			pTimerUs->TaskarrTab[ i ] = pTimerUs->TaskarrTab[ i + 1 ];
+			pTimerUs->TaskNumTab[ i ] = pTimerUs->TaskNumTab[ i + 1 ];
 		}
 		
-	}while( ( htimdelay.WorkNum > 0) && ( 0 == htimdelay.TaskarrTab[ 0 ]));	//当前要执行的ARR值为0,并且有任务数
+	}while( ( pTimerUs->WorkNum > 0) && ( 0 == pTimerUs->TaskarrTab[ 0 ]));	//当前要执行的ARR值为0,并且有任务数
 	
-	if( 0 != htimdelay.WorkNum )		//不只有一个延时任务时,启动下一个任务
+	if( 0 != pTimerUs->WorkNum )		//不只有一个延时任务时,启动下一个任务
 	{
-		__HAL_TIM_SET_AUTORELOAD( &TIMDelayhtim, htimdelay.TaskarrTab[ 0 ] );		//设置ARR值
-		__HAL_TIM_SET_COUNTER( &TIMDelayhtim, 0 );		//清零CNT
-		HAL_TIM_Base_Start_IT( &TIMDelayhtim);		//重新启动定时器
+        TimerUs_HAL_SetTimAutoReload( pTimerUs, pTimerUs->TaskarrTab[ 0 ] );		//设置ARR值
+		TimerUs_HAL_SetTimCounter( pTimerUs, 0 );   //清零CNT
+        TimerUs_HAL_StartTimIt( pTimerUs );         //重新启动定时器
 	}
 	else
 	{
-		HAL_TIM_Base_Stop_IT( &TIMDelayhtim );		//关闭定时器
+        TimerUs_HAL_StopTimIt( pTimerUs );		    //关闭定时器
 	}
 }
 
-//启用定时器延时，参数为：延时us数、是否使能中、断回调函数指针
-//函数范围有严格限定，us不得大于65534，不得小于2，TaskNum不得大于TIMDelay_TASK_NUMMAX
-HAL_StatusTypeDef TIMDelayStart( uint8_t TaskNum, uint16_t us, uint8_t use_it)
+/**
+  * @brief  启用定时器任务
+  * @note	参数范围限定，us不得大于65534，不得小于2，
+  * 		TaskNum不得大于TimerUs_TASK_NUMMAX
+  * @param  TaskNum	定时器任务序号
+  * @param  us		定时时间，单位us
+  * @param  use_it	是否使用中断回调
+  * @return 成功与否
+  * @retval 0	成功
+  * 		-1	失败
+  * 		-2	繁忙
+  */
+int8_t TimerUs_Start( TimerUs_TypeDef *pTimerUs, uint8_t TaskNum, uint16_t us, uint8_t use_it)
 {
 	uint16_t 	tmp;
 	uint8_t		i;
 	
-	if( TIMDelay_Busy == htimdelay.Task[ TaskNum ].state  )
-		return HAL_BUSY;
-	//htimdelay.Task[ TaskNum ].us = us;
-	htimdelay.Task[ TaskNum ].state = TIMDelay_Busy;
-	htimdelay.Task[ TaskNum ].use_it = use_it;
+	if( TIMERUS_BUSY == pTimerUs->Task[ TaskNum ].state  )
+		return -2;
+	//pTimerUs->Task[ TaskNum ].us = us;
+	pTimerUs->Task[ TaskNum ].state = TIMERUS_BUSY;
+	pTimerUs->Task[ TaskNum ].use_it = use_it;
 	
 	//根据定时器的状态，不同的插入功能形式
-	if( htimdelay.WorkNum != 0)		//如果处于工作状态
+	if( pTimerUs->WorkNum != 0)		//如果处于工作状态
 	{
-		TIMDelayhtim.Instance->CR1 &= ~(TIM_CR1_CEN);	//暂停CNT计数
-		__HAL_TIM_DISABLE_IT( &TIMDelayhtim, TIM_IT_UPDATE);	//关闭中断，防止中途改变
+        TimerUs_HAL_PauseTim( pTimerUs );	        //暂停CNT计数
+        TimerUs_HAL_DisableTimIt( pTimerUs );       //关闭中断使能，防止中途改变
 		
-		tmp = ( (TIMDelayhtim.Instance->ARR) - (TIMDelayhtim.Instance->CNT) + 1);	//TMP值含义为现在到定时器原本溢出的时间
+		tmp = ( ( TimerUs_HAL_GetTimAutoReload( pTimerUs ) ) - ( TimerUs_HAL_GetTimCounter( pTimerUs ) ) + 1);	//TMP值含义为现在到定时器原本溢出的时间
 		if( tmp > us)		//如果当前计时模式剩余时间大于插入任务所需要的时间，则将任务插入置最前端
 		{
-			TIMDelayhtim.Instance->CNT += tmp - us;		//将CNT值增加提前，使得从现在开始计时溢出的时间为本次插入任务的要求时间
-			__HAL_TIM_ENABLE( &TIMDelayhtim );		//恢复CNT计数
+            //将CNT值增加提前，使得从现在开始计时溢出的时间为本次插入任务的要求时间
+            TimerUs_HAL_SetTimCounter( pTimerUs, TimerUs_HAL_GetTimCounter( pTimerUs ) + tmp - us );
+            
+			TimerUs_HAL_EnableTim( pTimerUs );		//恢复CNT计数
 			
-			for( i = htimdelay.WorkNum; i > 0; --i )		//插入至最前端，所有任务后移
+			for( i = pTimerUs->WorkNum; i > 0; --i )		//插入至最前端，所有任务后移
 			{
-				htimdelay.TaskNumTab[ i ] = htimdelay.TaskNumTab[ i - 1 ];
-				htimdelay.TaskarrTab[ i ] = htimdelay.TaskarrTab[ i - 1 ];
+				pTimerUs->TaskNumTab[ i ] = pTimerUs->TaskNumTab[ i - 1 ];
+				pTimerUs->TaskarrTab[ i ] = pTimerUs->TaskarrTab[ i - 1 ];
 			}
-			htimdelay.TaskarrTab[ 1 ] = tmp - 1;	//原本处于工作的延时任务被滞后，滞后的ARR值为剩余时间值,而后续任务时间不变
-			htimdelay.TaskarrTab[ 0 ] = us - 1;		//该值不重要
-			htimdelay.TaskNumTab[ 0 ] = TaskNum;	//记录任务标号	
+			pTimerUs->TaskarrTab[ 1 ] = tmp - 1;	//原本处于工作的延时任务被滞后，滞后的ARR值为剩余时间值,而后续任务时间不变
+			pTimerUs->TaskarrTab[ 0 ] = us - 1;		//该值不重要
+			pTimerUs->TaskNumTab[ 0 ] = TaskNum;	//记录任务标号	
 		}
 		else	//否则任务将插入至从小到大的顺序中
 		{
-			__HAL_TIM_ENABLE( &TIMDelayhtim );		//恢复CNT计数
+			TimerUs_HAL_EnableTim( pTimerUs );		//恢复CNT计数
 			
-			for( i = 1, us -= tmp; i < htimdelay.WorkNum; ++i )		//任务依次从前向后累加比较
+			for( i = 1, us -= tmp; i < pTimerUs->WorkNum; ++i )		//任务依次从前向后累加比较
 			{
-				if( us < htimdelay.TaskarrTab[ i ] + 1)		//插入任务小于当前序列的时间值，则插入该序列
+				if( us < pTimerUs->TaskarrTab[ i ] + 1)		//插入任务小于当前序列的时间值，则插入该序列
 				{
-					for( tmp = htimdelay.WorkNum; tmp > i + 1; ++tmp)	//插入至序列i，i后所有任务后移，除了被插入点，其他后续任务序号后移，ARR不变
+					for( tmp = pTimerUs->WorkNum; tmp > i + 1; ++tmp)	//插入至序列i，i后所有任务后移，除了被插入点，其他后续任务序号后移，ARR不变
 					{
-						htimdelay.TaskNumTab[ tmp ] = htimdelay.TaskNumTab[ tmp - 1 ];
-						htimdelay.TaskarrTab[ tmp ] = htimdelay.TaskarrTab[ tmp - 1 ];
+						pTimerUs->TaskNumTab[ tmp ] = pTimerUs->TaskNumTab[ tmp - 1 ];
+						pTimerUs->TaskarrTab[ tmp ] = pTimerUs->TaskarrTab[ tmp - 1 ];
 					}
-					htimdelay.TaskarrTab[ i + 1 ] = htimdelay.TaskarrTab[ i ] - us;		//被插入的任务后移一个序号，且ARR值减去US
-					htimdelay.TaskNumTab[ i + 1 ] = htimdelay.TaskNumTab[ i ];
+					pTimerUs->TaskarrTab[ i + 1 ] = pTimerUs->TaskarrTab[ i ] - us;		//被插入的任务后移一个序号，且ARR值减去US
+					pTimerUs->TaskNumTab[ i + 1 ] = pTimerUs->TaskNumTab[ i ];
 					
-					htimdelay.TaskarrTab[ i ] = us - 1;		//新任务插入
-					htimdelay.TaskNumTab[ i ] = TaskNum;
+					pTimerUs->TaskarrTab[ i ] = us - 1;		//新任务插入
+					pTimerUs->TaskNumTab[ i ] = TaskNum;
 					i = 0;	//用i=0来标识，已找到插入点情况，
 					break;
 				}
 				else
 				{
-					us -= htimdelay.TaskarrTab[ i ] + 1;	//插入任务大于当前序列时间值，减去这个时间差，后移
+					us -= pTimerUs->TaskarrTab[ i ] + 1;	//插入任务大于当前序列时间值，减去这个时间差，后移
 				}
 			}
 			if( 0 != i )	//如果i不为0，说明没有找到插入点，则直接插入至最后
 			{
-				htimdelay.TaskarrTab[ htimdelay.WorkNum ] = us - 1;
-				htimdelay.TaskNumTab[ htimdelay.WorkNum ] = TaskNum;
+				pTimerUs->TaskarrTab[ pTimerUs->WorkNum ] = us - 1;
+				pTimerUs->TaskNumTab[ pTimerUs->WorkNum ] = TaskNum;
 			}
 		}
 	}
 	else		//不处于工作状态，直接放置第一序列
 	{
-		htimdelay.TaskarrTab[ 0 ] = us - 1;
-		htimdelay.TaskNumTab[ 0 ] = TaskNum;
-		__HAL_TIM_SET_AUTORELOAD( &TIMDelayhtim, us - 1 );		//设置ARR值
-		__HAL_TIM_SET_COUNTER( &TIMDelayhtim, 0 );		//清零CNT
-		__HAL_TIM_ENABLE( &TIMDelayhtim );				//启动定时器开始计数
+		pTimerUs->TaskarrTab[ 0 ] = us - 1;
+		pTimerUs->TaskNumTab[ 0 ] = TaskNum;
+        TimerUs_HAL_SetTimAutoReload( pTimerUs, us - 1 );		//设置ARR值
+        TimerUs_HAL_SetTimCounter( pTimerUs, 0 );		//清零CNT
+        TimerUs_HAL_EnableTim( pTimerUs );		//恢复CNT计数
 	}
-	++(htimdelay.WorkNum);	//任务数+1
+	++(pTimerUs->WorkNum);	//任务数+1
 	
-	__HAL_TIM_ENABLE_IT( &TIMDelayhtim, TIM_IT_UPDATE);		//开启中断
+	TimerUs_HAL_EnableTimIt( pTimerUs );		//开启中断
 	
-	return HAL_OK;
+	return 0;
 }
+
+///函数分组结束花括号
+/**
+  * @}
+  */
+
+///模块分组结束花括号
+/**
+  * @}
+  */
+
+/*************************** trzhongty@163.com **************END OF FILE*******/
--- timerus.h
+++ timerus.h
@@ -1,27 +1,65 @@
-#ifndef __TIMDELAY_H
-#define __TIMDELAY_H
+/**
+  ******************************************************************************
+  * @file    timerus.h
+  * @author  ThinkHome
+  * @brief   基于单个硬件定时器实现的多任务US级别中断回调软件定时器
+  ******************************************************************************
+  * @version 	0.1
+  * @date		2019年9月12日23:46:52
+  * @note		基于单个硬件定时器实现的多任务US级别中断回调软件定时器
+  * @since		初次创建
+  *
+  ******************************************************************************
+  */
 
-#include "stm32f0xx_hal.h"
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __TIMERUS_H__
+#define __TIMERUS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdint.h"
+
+/** @addtogroup TimerUs
+  * @{
+  */
+/* Exported constants --------------------------------------------------------*/
+/** @defgroup TimerUs Exported constants
+  * @{
+  */
 
 
-//移植配置相关
-//======================================================
-#define TIMDelayhtim			htim14		//所使用的定时器
-#define TIMDelay_TASK_NUMMAX		2			//总共提供两个互补干扰的延时任务
+/** @defgroup TimerUs 移植相关宏定义分组
+  * @{
+  */
 
-#define TIMDelay_TASK_0_WIFIAT		0		//给不同任务分配序号
-#define TIMDelay_TASK_1_UARTBUS		1
+#ifndef TIMERUS_TASK_NUMMAX
+    #define TIMERUS_TASK_NUMMAX		2		///<总共提供两个互补干扰的延时任务
+#endif
+
+/**
+  * @}
+  */
 
 
-//控制相关参数
-//======================================================
+ /**
+  * @}
+  */
+
+/* Exported types ------------------------------------------------------------*/
+/** @defgroup TimerUs Exported Types
+  * @{
+  */
 
 typedef enum
 {
-	TIMDelay_Ready 	= 0,
-	TIMDelay_Busy 	= 1,
-	TIMDelay_OK 	= 2
-}TIMDelay_state_enum;
+	TIMERUS_READY 	= 0,
+	TIMERUS_BUSY 	= 1,
+	TIMERUS_OK 	= 2
+}TimerUs_state_enum;
 
 //每个独立的延时任务所存储的数据
 typedef struct
@@ -30,30 +68,208 @@ typedef struct
 //	uint16_t	us;				//延时长度
 //	uint16_t	timarr;			//加载到ARR的值
 	uint8_t		use_it;			//非零为启用回调函数，否则不回调
-	volatile TIMDelay_state_enum state;		//工作状态
+	volatile TimerUs_state_enum state;		//工作状态
 	
-}TIMDelay_Task_TypeDef;
+}TimerUs_Task_TypeDef;
 
 typedef struct
 {
-	TIMDelay_Task_TypeDef	Task[ TIMDelay_TASK_NUMMAX ];	//每个任务的信息结构体数组
-	uint16_t	TaskNumTab[ TIMDelay_TASK_NUMMAX ];		//决定延时任务的执行顺序
-	uint16_t	TaskarrTab[ TIMDelay_TASK_NUMMAX ];	//每个延时任务的时间点,从小到大排序，对应的任务序号为TaskTab内的序号
+	TimerUs_Task_TypeDef	Task[ TIMERUS_TASK_NUMMAX ];	//每个任务的信息结构体数组
+	uint16_t	TaskNumTab[ TIMERUS_TASK_NUMMAX ];		//决定延时任务的执行顺序
+	uint16_t	TaskarrTab[ TIMERUS_TASK_NUMMAX ];	//每个延时任务的时间点,从小到大排序，对应的任务序号为TaskTab内的序号
 	
-	volatile uint8_t	WorkNum;							//在一次连续的定时器工作期间的任务总数
-//	volatile uint8_t	Taskp;								//指向TaskTab的序号
+    void              * TimHandle;      //定时器句柄(指针)  //用于方便对接
+	volatile uint8_t	WorkNum;        //在一次连续的定时器工作期间的任务总数
+    uint8_t             TaskNum;        //记录已注册的任务数量,与序号上限
 	
-}TIMDelay_TypeDef;
+}TimerUs_TypeDef;
+
+/**
+  * @}
+  */
+
+/* Exported macro ------------------------------------------------------------*/
+/** @defgroup TimerUs Exported Macros
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/* Exported functions --------------------------------------------------------*/
+/** @addtogroup TimerUs_Exported_Functions
+  * @{
+  */
+
+ /** @addtogroup TimerUs 需移植对接函数接口
+  * @{
+  */
+
+/**
+  * @brief  暂停硬件定时器计数
+  * @note	只暂停不重置
+  * @param  pTimerUs	主结构体指针
+  */
+void TimerUs_HAL_PauseTim( TimerUs_TypeDef *pTimerUs );
+
+/**
+  * @brief  恢复硬件定时器计数
+  * @note	只恢复不重置
+  * @param  pTimerUs	主结构体指针
+  */
+void TimerUs_HAL_EnableTim( TimerUs_TypeDef *pTimerUs );
+
+/**
+  * @brief  设置硬件定时器自动重载值
+  * @param  pTimerUs	主结构体指针
+  * 		AutoReload	自动重载值
+  */
+void TimerUs_HAL_SetTimAutoReload( TimerUs_TypeDef *pTimerUs , uint16_t AutoReload);
+
+/**
+  * @brief  获取硬件定时器自动重载值
+  * @param  pTimerUs	主结构体指针
+  */
+uint16_t TimerUs_HAL_GetTimAutoReload( TimerUs_TypeDef *pTimerUs);
+
+/**
+  * @brief  设置硬件定时器计数器值
+  * @param  pTimerUs	主结构体指针
+  * 		Cnt			计数器值
+  */
+void TimerUs_HAL_SetTimCounter( TimerUs_TypeDef *pTimerUs , uint16_t Cnt);
+
+/**
+  * @brief  获取硬件定时器计数值
+  * @param  pTimerUs	主结构体指针
+  */
+uint16_t TimerUs_HAL_GetTimCounter( TimerUs_TypeDef *pTimerUs);
+
+/**
+  * @brief  关闭硬件定时器并关闭中断
+  * @param  pTimerUs	主结构体指针
+  */
+void TimerUs_HAL_StopTimIt( TimerUs_TypeDef *pTimerUs );
+
+/**
+  * @brief  开启硬件定时器并开启中断
+  * @param  pTimerUs	主结构体指针
+  */
+void TimerUs_HAL_StartTimIt( TimerUs_TypeDef *pTimerUs );
+
+/**
+  * @brief  使能硬件定时器中断
+  * @param  pTimerUs	主结构体指针
+  */
+void TimerUs_HAL_EnableTimIt( TimerUs_TypeDef *pTimerUs );
+
+/**
+  * @brief  失能硬件定时器中断
+  * @param  pTimerUs	主结构体指针
+  */
+void TimerUs_HAL_DisableTimIt( TimerUs_TypeDef *pTimerUs );
+
+ /**
+  * @}
+  */
+
+ /** @addtogroup TimerUs_Exported_Functions
+  * @{
+  */
+
+/**
+  * @brief  硬件定时器中断函数
+  * @param  pTimerUs    定时器主结构体指针
+  */
+void TimerUs_IT( TimerUs_TypeDef *pTimerUs );
+
+/**
+  * @brief  初始化定时器结构体
+  * @param  pTimerUs    定时器主结构体指针
+  * @param  TimHandle   硬件定时器句柄（指针）
+  */
+ void TimerUs_Init( TimerUs_TypeDef *pTimerUs, void * TimHandle );
+
+/**
+  * @brief  创建一个定时任务
+  * @param  pTimerUs    定时器主结构体指针
+  * @param  pFun        定时中断回调函数,存在一个u8参数，指明任务序号
+  * @return 返回任务序号，小于0表示创建失败
+  */
+int8_t TimerUs_TaskCreate( TimerUs_TypeDef *pTimerUs, void ( *pFun)( uint8_t TaskNum ) );
+
+/**
+  * @brief  启用定时器任务
+  * @note	参数范围限定，us不得大于65534，不得小于2，
+  * 		TaskNum不得大于TimerUs_TASK_NUMMAX
+  * @param  TaskNum	定时器任务序号
+  * @param  us		定时时间，单位us
+  * @param  use_it	是否使用中断回调
+  * @return 成功与否
+  * @retval 0	成功
+  * 		-1	失败
+  * 		-2	繁忙
+  */
+int8_t TimerUs_Start( TimerUs_TypeDef *pTimerUs, uint8_t TaskNum, uint16_t us, uint8_t use_it);
 
 
 
-//宏函数和函数、全局变量声明
-//======================================================
+ /**
+  * @}
+  */
 
-extern TIMDelay_TypeDef htimdelay;
+/**
+  * @}
+  */
 
-HAL_StatusTypeDef TIMDelayInit( uint8_t TaskNum, void ( *pfun)() );
-void TIMDelayIT(void);
-HAL_StatusTypeDef TIMDelayStart( uint8_t TaskNum, uint16_t us, uint8_t use_it);
+/* Private types -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private constants ---------------------------------------------------------*/
+/** @defgroup TimerUs Private Constants
+  * @{
+  */
 
+/**
+  * @}
+  */
+
+/* Private macros ------------------------------------------------------------*/
+/** @defgroup TimerUs Private Macros
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/* Private functions ---------------------------------------------------------*/
+/** @defgroup TimerUs Private Functions
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+///TimerUs分组结束花括号
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* __TIMERUS_H__ */
+
+/*************************** trzhongty@163.com **************END OF FILE*******/
+
+
+
+
+
+
+
+
+
